c52da661cc868d8242252fd8c8c77a368b28ed77
Andrew Dunkman <adunkman@gmail.com>
2021-08-18 22:02:29 -0400

    Use A record for cloudfront; CNAME wildcard.
    Saw this suggested on Reddit, giving it a try: https://www.reddit.com/r/aws/comments/9xd7vv/is_it_possible_to_point_a_wildcard_dns_record_to/

--- terraform/route53/main.tf
+++ terraform/route53/main.tf
@@ -16,12 +16,19 @@ resource "aws_route53_record" "main" {
 
 resource "aws_route53_record" "preview" {
   zone_id = aws_route53_zone.zone.zone_id
-  name = "*.${var.domain_name}"
-  type = "CNAME"
+  name = "preview.${var.domain_name}"
+  type = "A"
 
   alias {
     name = var.preview_cloudfront_domain_name
     zone_id = var.preview_cloudfront_hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "wildcard" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name = "*.${var.domain_name}"
+  type = "CNAME"
+  records = ["preview.${var.domain_name}"]
 }
