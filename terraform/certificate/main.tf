b2aff6f2c323e778756f77c0a8583ad8f4149c3b
Andrew Dunkman <adunkman@gmail.com>
2021-08-18 20:09:31 -0400

    Specify second-level SAN for certificate.

--- terraform/certificate/main.tf
+++ terraform/certificate/main.tf
@@ -1,7 +1,10 @@
 resource "aws_acm_certificate" "certificate" {
   domain_name = var.domain_name
   validation_method = "DNS"
-  subject_alternative_names = [ "*.${var.domain_name}" ]
+  subject_alternative_names = [
+    "*.${var.domain_name}",
+    "*.preview.${var.domain_name}",
+  ]
 
   lifecycle {
     create_before_destroy = true
